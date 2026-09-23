285b5aff3de06a2ef62fc3b2205482ad6d32dc37
adelito <khalife1001@gmail.com>
2026-09-23 12:44:53 -0400

    base model

--- model.py
+++ model.py
@@ -9,6 +9,8 @@ import timm
 import matplotlib.pyplot as plt #for visualization
 import pandas as pd
 import numpy as np
+import sys
+from tqdm.notebook import tqdm
 
 class PlayingCardDataset(Dataset):
     def __init__(self, root_dir, transform=None):
@@ -31,14 +33,79 @@ transform = transforms.Compose([
     transforms.ToTensor()
 ])
 
-data_dir = 'dataset/train'
+train_folder = 'dataset/train'
+validation_folder = 'dataset/validation'
+test_folder = 'dataset/test'
 
+train_dataset = PlayingCardDataset(train_folder, transform=transform)
+validation_dataset = PlayingCardDataset(validation_folder, transform=transform)
+test_dataset = PlayingCardDataset(test_folder, transform=transform)
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+validation_loader = DataLoader(validation_dataset, batch_size=32, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+data_dir = 'dataset/train'
 dataset = PlayingCardDataset(root_dir=data_dir, transform=transform)
 image, label = dataset[100]
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 
-dataloader = DataLoader(dataset)
+for images, labels in dataloader:
+    break
+
+class SimpleCardClassifier(nn.Module):
+    def __init__(self, num_classes=53):
+        super().__init__()
+        self.base_model = timm.create_model('efficientnet_b0', pretrained=True)
+        self.features = nn.Sequential(*list(self.base_model.children())[:-1])
+        enet_out_size = 1280
+        self.classifier = nn.Linear(enet_out_size, num_classes)
+
+    def forward(self, x):
+        x = self.features(x)
+        output = self.classifier(x)
+
+        return output
 
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f'Using device: {device}')
 
+model = SimpleCardClassifier(num_classes=len(dataset.classes)).to(device)
 
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+num_epochs = 5
+train_losses, val_losses = [], []
+model.to(device)
+
+for epoch in range(num_epochs):
+    model.train()
+    running_loss = 0.0
+    for images, labels in tqdm(train_loader, desc="Training"):
+        images, labels = images.to(device), labels.to(device)
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item() * images.size(0)
+
+    train_losses.append(running_loss / len(train_loader.dataset))
+
+    model.eval()
+    val_loss = 0.0
+    with torch.no_grad():
+        for inputs, labels in tqdm(validation_loader, desc="Validation"):
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            val_loss += loss.item() * inputs.size(0)
+
+    val_losses.append(val_loss / len(validation_loader.dataset))
+
+    print(f'Epoch [{epoch+1}/{num_epochs}], Train Loss: {train_losses[-1]:.4f}, Validation Loss: {val_losses[-1]:.4f}')
 
